1c8725052081f2eacddcddf5973880f08407522f
Mayuko Murakami <mayuko3749@gmail.com>
2021-04-16 11:18:15 +0900

    ストップウォッチ

---
+++ 1_workspace/1/□と○/main.js
@@ -0,0 +1,12 @@
+const figure = document.getElementById("figure")
+
+const squareButton = document.getElementById("square-button")
+const circleButton = document.getElementById("circle-button")
+
+circleButton.onclick = function() {
+  figure.classList.add("rounded")
+}
+
+squareButton.onclick = function() {
+  figure.classList.remove("rounded")
+}
