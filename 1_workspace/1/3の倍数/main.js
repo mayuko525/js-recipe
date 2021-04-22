48b8b851a115f54677a02dbcc6569a6f5a7df2fa
Mayuko Murakami <mayuko3749@gmail.com>
2021-04-22 13:04:48 +0900

    Trello(保存機能なしをやりました)

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -7,3 +7,19 @@ const genkiFunction = function(number) {
     }
   }
 }
+
+const fizzbuzzFunction = function(number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 15 === 0) {
+      console.log("fizzbuzz")
+    } else if (n % 5 === 0) {
+      console.log("buzz")
+    } else if (n % 3 === 0) {
+      console.log("fizz")
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+fizzbuzzFunction(100)
---
+++ 3_semi/week2/object.js
@@ -0,0 +1,7 @@
+const mayuko = {
+  name: "まゆこ",
+  live: "東京",
+  study: ["法制史", "英語", "フランス語"],
+  hobby: ["ラジオ", "Netflix", "刺繍"],
+  sayHello: function() {},
+}
